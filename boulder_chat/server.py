6801db8ed5d1507f0893a79271700d1c02205f43
jonah-hooper <jonah.graham.hooper@gmail.com>
2017-05-22 17:43:39 +0200

    Created server objects

--- boulder_chat/server.py
+++ boulder_chat/server.py
@@ -2,5 +2,13 @@
 The code to construct the server will be here
 """
 
-def a_method():
-    return True
+class User(object):
+    public_key: str
+    user_name: str
+    user_status: str
+    messages: List[Messages] # this may change to a queue in future
+
+class Server(object):
+    private_key: str
+    users: List[User]
+
