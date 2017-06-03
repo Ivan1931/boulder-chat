fece2454c4ddc6ba4881f4365cd6f27857d7b0d9
William Grant <willmgrant@gmail.com>
2017-06-03 14:19:03 +0200

    got encryption and decryption with padding!

--- boulder_chat/crypto.py
+++ boulder_chat/crypto.py
@@ -8,28 +8,31 @@ from json import dumps
 block_size = 16
 IV = block_size * '\x00'
 mode = AES.MODE_CBC
-message =
+message = 'bitches'
 password = 'abcdefghijklmnopqrstuvwxyz'
 test_key = hashlib.sha256(password.encode()).digest()
 
 # padding methods
-# pad = lambda s: s + (block_size - len(s) % block_size) * chr(block_size - len(s) % block_size).encode()
-
+pad = lambda s: s + (block_size - len(s) % block_size) * chr(block_size - len(s) % block_size).encode()
+unpad = lambda s: s[:-ord(s[len(s)-1:])]
 # generic encryption method
 def encrypt(key, payload):
+    payload = payload.encode();
+    raw_payload = pad(payload)
     # generate the cipher for encryption and encrypt
     cipher = AES.new(key, mode, IV=IV)
-    encoded_payload = cipher.encrypt(payload)
+    encoded_payload = cipher.encrypt(raw_payload)
     # decode back into plain text and return
-    return encoded_payload
+    return base64.b64encode(encoded_payload).decode('utf-8')
 
 # generic decryption method
 def decrypt(key, ecrypted_payload):
+    ecrypted_payload = base64.b64decode(ecrypted_payload)
     # generate the cipher for decryption and decrypt
     cipher = AES.new(key, mode, IV=IV)
     decrypted_payload = cipher.decrypt(ecrypted_payload)
     # decode back into plain text, unpad and return
-    return decrypted_payload
+    return unpad(decrypted_payload).decode('utf-8')
 
 # def public_key_encrypt(public_key: str, payload: str) -> str:
 #     """
@@ -38,13 +41,11 @@ def decrypt(key, ecrypted_payload):
 #     raise NotImplementedError()
 #
 def private_key_encrypt(private_key, payload):
-    payload = payload.encode()
     result = encrypt(private_key, payload)
     return result
 
 def private_key_decrypt(private_key, payload):
     result = decrypt(private_key, payload)
-    result = result.decode()
     return result
 #
 # def public_key_decrypt(public_key: str, payload: str) -> str:
